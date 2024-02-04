c85895b57dd7f54bcaf9f1f406ddb51035d534cf
mackavelli1215 <mackenzieellis@gmail.com>
2024-02-04 10:45:06 -0500

    streamlit changes

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import langchain_logic as lch
 import streamlit as st
 
+st.sidebar.image('.\images\sprink_logo.jpg', width= 240) 
 st.title("Sprink Quote Generator")
+st.date_input("Todays date")
+st.slider('Slide me', min_value=0, max_value=10)
